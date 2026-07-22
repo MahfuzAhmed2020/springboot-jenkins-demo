ea5ece43fd9c84090647dda7fcb7cd730bc9412d
MAhmed <68579823+MahfuzAhmed2020@users.noreply.github.com>
2026-07-22 14:59:12 -0400

    Implement CRUD operations for students in script.js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,22 @@
-function save(){
+const url="http://localhost:8080/students";
 
-fetch("/student",{
+
+// CREATE
+
+function addStudent(){
+
+let student={
+
+name:document.getElementById("name").value,
+
+email:document.getElementById("email").value,
+
+age:parseInt(document.getElementById("age").value)
+
+};
+
+
+fetch(url,{
 
 method:"POST",
 
@@ -8,16 +24,199 @@ headers:{
 "Content-Type":"application/json"
 },
 
-body:JSON.stringify({
+body:JSON.stringify(student)
+
+})
+.then(res=>res.json())
+.then(data=>{
+
+alert("Student Added");
+
+loadStudents();
+
+});
+
+
+}
+
+
+
+// READ
+
+function loadStudents(){
+
+
+fetch(url)
+
+.then(res=>res.json())
+
+.then(data=>{
+
+
+let rows="";
+
+
+data.forEach(s=>{
+
+
+rows += `
+
+<tr>
+
+<td>${s.id}</td>
+
+<td>${s.name}</td>
+
+<td>${s.email}</td>
+
+<td>${s.age}</td>
+
+</tr>
+
+
+`;
+
+
+});
+
+
+document.getElementById("studentTable").innerHTML=rows;
+
+
+});
+
+
+}
+
+
+
+// PUT UPDATE
+
+function updateStudent(){
+
+
+let id=document.getElementById("id").value;
+
+
+let student={
+
+
+name:document.getElementById("name").value,
+
+
+email:document.getElementById("email").value,
+
+
+age:parseInt(document.getElementById("age").value)
+
+
+};
+
+
+
+fetch(url+"/"+id,{
+
+method:"PUT",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify(student)
+
+
+})
+
+.then(res=>res.json())
+
+.then(()=>{
+
+
+alert("Updated");
+
+loadStudents();
+
+
+});
+
+
+}
+
+
+
+// PATCH
+
+function patchStudent(){
+
+
+let id=document.getElementById("id").value;
+
+
+
+let student={
 
 name:document.getElementById("name").value
 
-})
+};
+
+
+
+fetch(url+"/"+id,{
+
+method:"PATCH",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify(student)
 
 })
 
-.then(r=>r.json())
 
-.then(d=>alert("Saved : "+d.name));
+.then(res=>res.json())
+
+.then(()=>{
+
+
+alert("Patched");
+
+loadStudents();
+
+
+});
+
+
+}
+
+
+
+
+// DELETE
+
+
+function deleteStudent(){
+
+
+let id=document.getElementById("id").value;
+
+
+fetch(url+"/"+id,{
+
+method:"DELETE"
+
+})
+
+
+.then(()=>{
+
+
+alert("Deleted");
+
+loadStudents();
+
+
+});
+
 
 }
