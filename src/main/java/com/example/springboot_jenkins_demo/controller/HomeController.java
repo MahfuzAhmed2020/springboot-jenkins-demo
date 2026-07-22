9200f1768269a41e9d7e90f17a80745e3600dbd6
mahfuz <20mahfuzahmed20@gmail.com>
2026-07-22 13:36:01 -0400

    Update home message

--- src/main/java/com/example/springboot_jenkins_demo/controller/HomeController.java
+++ src/main/java/com/example/springboot_jenkins_demo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.springboot_jenkins_demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,9 @@ public class HomeController {
     public Student add(@RequestBody Student s) {
         return service.save(s);
     }
+    @GetMapping("/")
+public String home() {
+    return "Hello Spring Boot";
+}
 
 }
